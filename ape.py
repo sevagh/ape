c0cb586b2898897ca5b8bd51a2bfea96bdf68e80
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-09-06 14:34:22 -0400

    Drop functionality successfully re-implemented

--- ape.py
+++ ape.py
@@ -9,7 +9,57 @@ import argparse
 import os
 import sys
 import logging
+from struct import unpack
+from binascii import unhexlify
 from prometheus_client import start_http_server, Counter, Gauge
+import datetime, threading
+
+
+STOP_DROP_STATS = False
+
+
+def drop_stats_output_reader(device, port, pinned_map_name, stats_interval_s, total_metric, manipulated_metric):
+    total_pkts = subprocess.check_output(
+            [
+                'bpftool',
+                '--bpffs',
+                'map',
+                'lookup',
+                'pinned',
+                pinned_map_name,
+                'key',
+                '1',
+                '0',
+                '0',
+                '0'
+            ])
+    dropped_pkts = subprocess.check_output(
+            [
+                'bpftool',
+                '--bpffs',
+                'map',
+                'lookup',
+                'pinned',
+                pinned_map_name,
+                'key',
+                '0',
+                '0',
+                '0',
+                '0'
+            ])
+    total_packets = unpack('<2i', unhexlify(
+        total_pkts.split(b'value:')[-1][1:-1].decode().replace(' ', '').encode('utf-8')
+    ))[0]
+    total_metric.labels(device, 'drop', port).set(total_packets)
+    dropped_packets = unpack('<2i', unhexlify(
+        dropped_pkts.split(b'value:')[-1][1:-1].decode().replace(' ', '').encode('utf-8')
+    ))[0]
+    manipulated_metric.labels(device, 'drop', port).set(dropped_packets)
+    global STOP_DROP_STATS
+    if not STOP_DROP_STATS:
+        threading.Timer(stats_interval_s, drop_stats_output_reader, [device, port, pinned_map_name, stats_interval_s, total_metric, manipulated_metric]).start()
+    else:
+        print('Stopping drop stats thread')
 
 
 def compile_xdp_kern_prog(makerule, cflags):
@@ -26,20 +76,45 @@ def compile_xdp_kern_prog(makerule, cflags):
 
 
 def main():
+    if os.geteuid() != 0:
+        print(
+            "Start {0} as root, it will drop privileges when compiling .o files".format(
+                sys.argv[0]
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    sudo_uid = int(os.getenv("SUDO_GID"))
+    sudo_gid = int(os.getenv("SUDO_UID"))
+
     parser = argparse.ArgumentParser(description="ape - network chaos tool")
 
     parser.add_argument("device", type=str, help="iface to attach XDP to")
     parser.add_argument(
-        "--udp-drop", dest="udp_drop", type=int, help="% of UDP packets to drop"
+        "--udp-drop", dest="udp_drop", type=int, help="%% of UDP packets to drop"
     )
     parser.add_argument(
         "--udp-port", dest="udp_port", type=int, help="UDP port to drop packets on"
     )
-
+    parser.add_argument(
+        "--listen-port",
+        dest="prom_port",
+        type=int,
+        help="HTTP port for Prometheus metrics endpoint",
+        default=8000,
+    )
+    parser.add_argument(
+        "--stats-interval-s",
+        dest="stats_interval_s",
+        type=int,
+        help="Time in s to poll XDP maps and generate Prometheus metrics",
+        default=1,
+    )
     parser.add_argument(
         "-d",
         "--debug",
-        help="Print lots of debugging statements",
+        help="debug out",
         action="store_const",
         dest="loglevel",
         const=logging.DEBUG,
@@ -48,7 +123,7 @@ def main():
     parser.add_argument(
         "-v",
         "--verbose",
-        help="Be verbose",
+        help="verbose out",
         action="store_const",
         dest="loglevel",
         const=logging.INFO,
@@ -59,19 +134,41 @@ def main():
         "XDP kernel programs loaded and attached by ape",
         ["interface", "name"],
     )
+    ape_total_metric = Gauge(
+        "ape_total_udp_packets",
+        "UDP packets intercepted by ape",
+        ["device", "action", "port"]
+    )
+    ape_manipulated_metric = Gauge(
+        "ape_manipulated_udp_packets",
+        "UDP packets manipulated by ape",
+        ["device", "action", "port"]
+    )
 
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
 
     run_drop = False
     unload_cmds = []
+    procs = []
 
     if args.udp_drop:
         cflags = ["-DUDP_DROP_PROB={0}".format(args.udp_drop)]
         run_drop = True
         if args.udp_port:
             cflags.append("-DUDP_PORT={0}".format(args.udp_port))
+        else:
+            args.udp_port = -1
+
+        # drop privileges
+        os.setresgid(sudo_gid, sudo_gid, -1)
+        os.setresuid(sudo_uid, sudo_uid, -1)
+
         compile_xdp_kern_prog("kern_drop", cflags)
+
+        # get them back
+        os.setresgid(0, 0, -1)
+        os.setresuid(0, 0, -1)
 
     if run_drop:
         print(
@@ -93,17 +190,20 @@ def main():
             ["./xdp_user_drop", "--auto-mode", "--unload", "--dev", args.device]
         )
 
+        print("Starting bpftool map listener in thread")
+        drop_stats_output_reader(args.device, args.udp_port, '/sys/fs/bpf/{0}/drop_count'.format(args.device), args.stats_interval_s, ape_total_metric, ape_manipulated_metric)
+
     def signal_handler(signal, frame):
-        print("we got Ctrl-C")
+        global STOP_DROP_STATS
+        STOP_DROP_STATS = True
         for u in unload_cmds:
             print(subprocess.check_output(u))
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    print("started prometheus metrics server at http://localhost:8000")
-    start_http_server(8000)
-    print("here")
+    print("Started prometheus metrics server at http://localhost:8000")
+    start_http_server(args.prom_port)
 
     event = threading.Event()
     event.wait()
--- xdp_user_drop.c
+++ xdp_user_drop.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 static const char *__doc__ =
-    "XDP loader\n"
-    " - Allows selecting BPF section --progsec name to XDP-attach to --dev\n";
+	"XDP loader\n"
+	" - Allows selecting BPF section --progsec name to XDP-attach to --dev\n";
 
 #include <errno.h>
 #include <getopt.h>
@@ -28,40 +28,42 @@ static const char *default_filename = "xdp_prog_kern.o";
 
 static const struct option_wrapper long_options[] = {
 
-    {{"help", no_argument, NULL, 'h'}, "Show help", false},
+	{ { "help", no_argument, NULL, 'h' }, "Show help", false },
 
-    {{"dev", required_argument, NULL, 'd'},
-     "Operate on device <ifname>",
-     "<ifname>",
-     true},
+	{ { "dev", required_argument, NULL, 'd' },
+	  "Operate on device <ifname>",
+	  "<ifname>",
+	  true },
 
-    {{"skb-mode", no_argument, NULL, 'S'},
-     "Install XDP program in SKB (AKA generic) mode"},
+	{ { "skb-mode", no_argument, NULL, 'S' },
+	  "Install XDP program in SKB (AKA generic) mode" },
 
-    {{"native-mode", no_argument, NULL, 'N'},
-     "Install XDP program in native mode"},
+	{ { "native-mode", no_argument, NULL, 'N' },
+	  "Install XDP program in native mode" },
 
-    {{"auto-mode", no_argument, NULL, 'A'}, "Auto-detect SKB or native mode"},
+	{ { "auto-mode", no_argument, NULL, 'A' },
+	  "Auto-detect SKB or native mode" },
 
-    {{"force", no_argument, NULL, 'F'},
-     "Force install, replacing existing program on interface"},
+	{ { "force", no_argument, NULL, 'F' },
+	  "Force install, replacing existing program on interface" },
 
-    {{"unload", no_argument, NULL, 'U'},
-     "Unload XDP program instead of loading"},
+	{ { "unload", no_argument, NULL, 'U' },
+	  "Unload XDP program instead of loading" },
 
-    {{"reuse-maps", no_argument, NULL, 'M'}, "Reuse pinned maps"},
+	{ { "reuse-maps", no_argument, NULL, 'M' }, "Reuse pinned maps" },
 
-    {{"quiet", no_argument, NULL, 'q'}, "Quiet mode (no output)"},
+	{ { "quiet", no_argument, NULL, 'q' }, "Quiet mode (no output)" },
 
-    {{"filename", required_argument, NULL, 1},
-     "Load program from <file>",
-     "<file>"},
+	{ { "filename", required_argument, NULL, 1 },
+	  "Load program from <file>",
+	  "<file>" },
 
-    {{"progsec", required_argument, NULL, 2},
-     "Load program in <section> of the ELF file",
-     "<section>"},
+	{ { "progsec", required_argument, NULL, 2 },
+	  "Load program in <section> of the ELF file",
+	  "<section>" },
 
-    {{0, 0, NULL, 0}, NULL, false}};
+	{ { 0, 0, NULL, 0 }, NULL, false }
+};
 
 #ifndef PATH_MAX
 #define PATH_MAX 4096
@@ -71,99 +73,103 @@ const char *pin_basedir = "/sys/fs/bpf";
 const char *map_name = "drop_count";
 
 /* Pinning maps under /sys/fs/bpf in subdir */
-int pin_maps_in_bpf_object(struct bpf_object *bpf_obj, struct config *cfg) {
-  char map_filename[PATH_MAX];
-  int err, len;
+int pin_maps_in_bpf_object(struct bpf_object *bpf_obj, struct config *cfg)
+{
+	char map_filename[PATH_MAX];
+	int err, len;
 
-  len = snprintf(map_filename, PATH_MAX, "%s/%s/%s", pin_basedir, cfg->ifname,
-                 map_name);
-  if (len < 0) {
-    fprintf(stderr, "ERR: creating map_name\n");
-    return EXIT_FAIL_OPTION;
-  }
+	len = snprintf(map_filename, PATH_MAX, "%s/%s/%s", pin_basedir,
+		       cfg->ifname, map_name);
+	if (len < 0) {
+		fprintf(stderr, "ERR: creating map_name\n");
+		return EXIT_FAIL_OPTION;
+	}
 
-  /* Existing/previous XDP prog might not have cleaned up */
-  if (access(map_filename, F_OK) != -1) {
-    if (verbose)
-      printf(" - Unpinning (remove) prev maps in %s/\n", cfg->pin_dir);
+	/* Existing/previous XDP prog might not have cleaned up */
+	if (access(map_filename, F_OK) != -1) {
+		if (verbose)
+			printf(" - Unpinning (remove) prev maps in %s/\n",
+			       cfg->pin_dir);
 
-    /* Basically calls unlink(3) on map_filename */
-    err = bpf_object__unpin_maps(bpf_obj, cfg->pin_dir);
-    if (err) {
-      fprintf(stderr, "ERR: UNpinning maps in %s\n", cfg->pin_dir);
-      return EXIT_FAIL_BPF;
-    }
-  }
-  if (verbose)
-    printf(" - Pinning maps in %s/\n", cfg->pin_dir);
+		/* Basically calls unlink(3) on map_filename */
+		err = bpf_object__unpin_maps(bpf_obj, cfg->pin_dir);
+		if (err) {
+			fprintf(stderr, "ERR: UNpinning maps in %s\n",
+				cfg->pin_dir);
+			return EXIT_FAIL_BPF;
+		}
+	}
+	if (verbose)
+		printf(" - Pinning maps in %s/\n", cfg->pin_dir);
 
-  /* This will pin all maps in our bpf_object */
-  err = bpf_object__pin_maps(bpf_obj, cfg->pin_dir);
-  if (err)
-    return EXIT_FAIL_BPF;
+	/* This will pin all maps in our bpf_object */
+	err = bpf_object__pin_maps(bpf_obj, cfg->pin_dir);
+	if (err)
+		return EXIT_FAIL_BPF;
 
-  return 0;
+	return 0;
 }
 
-int main(int argc, char **argv) {
-  struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+int main(int argc, char **argv)
+{
+	struct rlimit r = { RLIM_INFINITY, RLIM_INFINITY };
 
-  if (setrlimit(RLIMIT_MEMLOCK, &r)) {
-    perror("setrlimit(RLIMIT_MEMLOCK)");
-    return 1;
-  }
+	if (setrlimit(RLIMIT_MEMLOCK, &r)) {
+		perror("setrlimit(RLIMIT_MEMLOCK)");
+		return 1;
+	}
 
-  struct bpf_object *bpf_obj;
-  int err, len;
+	struct bpf_object *bpf_obj;
+	int err, len;
 
-  struct config cfg = {
-      .xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
-      .ifindex = -1,
-      .do_unload = false,
-  };
-  /* Set default BPF-ELF object file and BPF program name */
-  strncpy(cfg.filename, default_filename, sizeof(cfg.filename));
-  /* Cmdline options can change progsec */
-  parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
+	struct config cfg = {
+		.xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_DRV_MODE,
+		.ifindex = -1,
+		.do_unload = false,
+	};
+	/* Set default BPF-ELF object file and BPF program name */
+	strncpy(cfg.filename, default_filename, sizeof(cfg.filename));
+	/* Cmdline options can change progsec */
+	parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
 
-  /* Required option */
-  if (cfg.ifindex == -1) {
-    fprintf(stderr, "ERR: required option --dev missing\n\n");
-    usage(argv[0], __doc__, long_options, (argc == 1));
-    return EXIT_FAIL_OPTION;
-  }
-  if (cfg.do_unload) {
-    if (!cfg.reuse_maps) {
-      /* TODO: Miss unpin of maps on unload */
-    }
-    return xdp_link_detach(cfg.ifindex, cfg.xdp_flags, 0);
-  }
+	/* Required option */
+	if (cfg.ifindex == -1) {
+		fprintf(stderr, "ERR: required option --dev missing\n\n");
+		usage(argv[0], __doc__, long_options, (argc == 1));
+		return EXIT_FAIL_OPTION;
+	}
+	if (cfg.do_unload) {
+		if (!cfg.reuse_maps) {
+			/* TODO: Miss unpin of maps on unload */
+		}
+		return xdp_link_detach(cfg.ifindex, cfg.xdp_flags, 0);
+	}
 
-  len = snprintf(cfg.pin_dir, PATH_MAX, "%s/%s", pin_basedir, cfg.ifname);
-  if (len < 0) {
-    fprintf(stderr, "ERR: creating pin dirname\n");
-    return EXIT_FAIL_OPTION;
-  }
+	len = snprintf(cfg.pin_dir, PATH_MAX, "%s/%s", pin_basedir, cfg.ifname);
+	if (len < 0) {
+		fprintf(stderr, "ERR: creating pin dirname\n");
+		return EXIT_FAIL_OPTION;
+	}
 
-  bpf_obj = load_bpf_and_xdp_attach(&cfg);
-  if (!bpf_obj)
-    return EXIT_FAIL_BPF;
+	bpf_obj = load_bpf_and_xdp_attach(&cfg);
+	if (!bpf_obj)
+		return EXIT_FAIL_BPF;
 
-  if (verbose) {
-    printf("Success: Loaded BPF-object(%s) and used section(%s)\n",
-           cfg.filename, cfg.progsec);
-    printf(" - XDP prog attached on device:%s(ifindex:%d)\n", cfg.ifname,
-           cfg.ifindex);
-  }
+	if (verbose) {
+		printf("Success: Loaded BPF-object(%s) and used section(%s)\n",
+		       cfg.filename, cfg.progsec);
+		printf(" - XDP prog attached on device:%s(ifindex:%d)\n",
+		       cfg.ifname, cfg.ifindex);
+	}
 
-  /* Use the --dev name as subdir for exporting/pinning maps */
-  if (!cfg.reuse_maps) {
-    err = pin_maps_in_bpf_object(bpf_obj, &cfg);
-    if (err) {
-      fprintf(stderr, "ERR: pinning maps\n");
-      return err;
-    }
-  }
+	/* Use the --dev name as subdir for exporting/pinning maps */
+	if (!cfg.reuse_maps) {
+		err = pin_maps_in_bpf_object(bpf_obj, &cfg);
+		if (err) {
+			fprintf(stderr, "ERR: pinning maps\n");
+			return err;
+		}
+	}
 
-  return EXIT_OK;
+	return EXIT_OK;
 }
